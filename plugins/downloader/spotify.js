552c299061f6110b84bf965ee6fb0ccc23ca5496
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-04-27 06:58:45 +0700

    Update spotify.js

--- plugins/downloader/spotify.js
+++ plugins/downloader/spotify.js
@@ -11,192 +11,86 @@ let api = 'https://spotifyapi.caliphdev.com'
 let handler = async (m, {
     conn,
     Func,
+    Scraper,
     text
 }) => {
-    switch (m.command) {
-        case 'spotifyplay': {
-            if (!text) throw '⚠️ Masukan Nama Lagu !'
-            const {
-                data: search
-            } = await axios(api + '/api/search/tracks', {
-                post: 'GET',
-                params: {
-                    q: text
-                }
-            });
-            const {
-                data: detail
-            } = await axios(api + '/api/info/track', {
-                post: 'GET',
-                params: {
-                    url: search[0].url
-                }
-            });
-            let linkurl;
-            try {
-                const {
-                    data: downloader
-                } = await axios.post('https://spotydown.media/api/download-track', {
-                    url: detail.url
-                }, {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    }
-                })
-                linkurl = downloader.file_url
-            } catch (e) {
-                try {
-                    linkurl = `${api + '/api/download/track?url=' + detail.url}`
-                } catch (e) {}
+    if (!text) throw '⚠️ Masukan Link/Query !'
+    if (Func.isUrl(text)) {
+        if (!/open.spotify.com/.test(text)) throw '⚠️Mana Link Spotify Nya !';
+        const {
+            data: detail
+        } = await axios(api + '/api/info/track', {
+            post: 'GET',
+            params: {
+                url: text
             }
-            const caption = `📁 Spotify Downloader
+        });
+        let linkurl;
+
+        try {
+            const { result: spdl } = await Scraper.spotiDown(detail.url)
+            linkurl = spdl.download
+        } catch (e) {
+            try {
+                linkurl = `${api + '/api/download/track?url=' + detail.url}`
+            } catch (e) {}
+        }
+        const caption = `📁 Spotify Downloader
 > • *Title:* ${detail.title || ''}
 > • *Artist:* ${detail.artist || ''}
 > • *Album:* ${detail.album || ''}
 > • *Url:* ${detail.url || ''}
-> • *Link-Download:* ${linkurl}`;
-            m.reply(caption);
-            let audio;
+> • *Link-Download:* ${linkurl || ''}`;
+        m.reply(caption);
+        let audio;
+        try {
+            const { result: spdl } = await Scraper.spotiDown(detail.url)
+            audio = { url: spdl.download }
+        } catch (e) {
             try {
                 const {
-                    data: downloader
-                } = await axios.post('https://spotydown.media/api/download-track', {
-                    url: detail.url
-                }, {
-                    headers: {
-                        Accept: "application/json",
-                        "Content-Type": "application/json"
-                    }
-                })
-                audio = {
-                    url: downloader.file_url
-                }
-            } catch (e) {
-                try {
-                    const {
-                        data
-                    } = await axios(api + '/api/download/track', {
-                        post: 'GET',
-                        params: {
-                            url: detail.url
-                        },
-                        responseType: 'arraybuffer'
-                    });
-                    audio = data
-                } catch (e) {}
-            }
-
-            conn.sendMessage(m.chat, {
-                audio,
-                mimetype: 'audio/mpeg'
-            }, {
-                quoted: m
-            });
-        }
-        break
-        case 'spotify':
-        case 'spdl': {
-            if (!text) throw '⚠️ Masukan Link/Query !'
-            if (Func.isUrl(text)) {
-                if (!/open.spotify.com/.test(text)) throw '⚠️Mana Link Spotify Nya !';
-                const {
-                    data: detail
-                } = await axios(api + '/api/info/track', {
+                    data
+                } = await axios(api + '/api/download/track', {
                     post: 'GET',
                     params: {
-                        url: text
-                    }
-                });
-                let linkurl;
-                try {
-                    const {
-                        data: downloader
-                    } = await axios.post('https://spotydown.media/api/download-track', {
                         url: detail.url
-                    }, {
-                        headers: {
-                            Accept: "application/json",
-                            "Content-Type": "application/json"
-                        }
-                    })
-                    linkurl = downloader.file_url
-                } catch (e) {
-                    try {
-                        linkurl = `${api + '/api/download/track?url=' + detail.url}`
-                    } catch (e) {}
-                }
-                const caption = `📁 Spotify Downloader
-> • *Title:* ${detail.title || ''}
-> • *Artist:* ${detail.artist || ''}
-> • *Album:* ${detail.album || ''}
-> • *Url:* ${detail.url || ''}
-> • *Link-Download:* ${linkurl}`;
-                m.reply(caption);
-                let audio;
-                try {
-                    const {
-                        data: downloader
-                    } = await axios.post('https://spotydown.media/api/download-track', {
-                        url: detail.url
-                    }, {
-                        headers: {
-                            Accept: "application/json",
-                            "Content-Type": "application/json"
-                        }
-                    })
-                    audio = {
-                        url: downloader.file_url
-                    }
-                } catch (e) {
-                    try {
-                        const {
-                            data
-                        } = await axios(api + '/api/download/track', {
-                            post: 'GET',
-                            params: {
-                                url: detail.url
-                            },
-                            responseType: 'arraybuffer'
-                        });
-                        audio = data
-                    } catch (e) {}
-                }
-
-                conn.sendMessage(m.chat, {
-                    audio,
-                    mimetype: 'audio/mpeg'
-                }, {
-                    quoted: m
+                    },
+                    responseType: 'arraybuffer'
                 });
-            } else {
-                const {
-                    data: search
-                } = await axios(api + '/api/search/tracks', {
-                    post: 'GET',
-                    params: {
-                        q: text
-                    }
-                });
-                if (!search && !search.length > 0) throw '⚠️ Maaf Lagu Yg Anda Search Tidak Di Temukan';
-
-                let message = `🔍 Search Spotify\n\n`;
-                message += search.map((a, i) => `\`[ ${i + 1} ]\`\n> • Title: ${a.title}\n> • Artist: ${a.artist}\n> • Url: ${a.url}`).join("\n\n");
-                await conn.sendAliasMessage(m.chat, {
-                    text: message
-                }, search.map((a, i) => ({
-                    alias: `${i + 1}`,
-                    response: `${m.prefix + m.command} ${a.url}`
-                })), m);
-            }
+                audio = await Buffer.from(data)
+            } catch (e) {}
         }
-        break
-    };
+
+        conn.sendMessage(m.chat, {
+            audio,
+            mimetype: 'audio/mpeg'
+        }, {
+            quoted: m
+        });
+    } else {
+        const {
+            data: search
+        } = await axios(api + '/api/search/tracks', {
+            post: 'GET',
+            params: {
+                q: text
+            }
+        });
+        if (!search && !search.length > 0) throw '⚠️ Maaf Lagu Yg Anda Search Tidak Di Temukan';
+        let message = `🔍 Search Spotify\n\n`;
+        message += search.map((a, i) => `\`[ ${i + 1} ]\`\n> • Title: ${a.title}\n> • Artist: ${a.artist}\n> • Url: ${a.url}`).join("\n\n");
+        await conn.sendAliasMessage(m.chat, {
+            text: message
+        }, search.map((a, i) => ({
+            alias: `${i + 1}`,
+            response: `${m.prefix + m.command} ${a.url}`
+        })), m);
+    }
 };
 
-handler.help = ["spotify", "spdl", "spotifyplay"].map(v => v + ' *[ Download/Search Lagu ]* ');
+handler.help = ["spotify", "spdl"].map(v => v + ' *[ Download/Search Lagu ]* ');
 handler.tags = ["downloader", "internet"];
-handler.command = ["spotify", "spdl", "spotifyplay"];
+handler.command = ["spotify", "spdl"];
 handler.limit = true;
 handler.loading = true;
 
