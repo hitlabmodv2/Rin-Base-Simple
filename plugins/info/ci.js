871dd3bfe330efde2269202af212fdad64a912f5
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-04-29 00:53:41 +0700

    Create ci.js

---
+++ plugins/info/ci.js
@@ -0,0 +1,33 @@
+// © HanakoBotz
+// • By: Leooxzy - Deku
+// • Owner: 6283136099660
+
+// By: Leooxzy
+// Bio cr: Krz
+
+module.exports = {
+    help: ['cekidch', 'ci'],
+    command: ['cekidch', 'ci'],
+    tags: ['info'],
+    loading: true,
+    code: async (m, { ctx, text }) => {
+        if (!/whatsapp.com\/channel/.test(text)) throw '⚠️ Mana Link Channel Nya !';
+        let id = text.replace(new RegExp(/https:\/\/\whatsapp.com\/channel\//, "gi"), "");
+        await ctx.newsletterMetadata('invite', id).then(async (a) => {
+            let caption = `𒁍⛶  \`Cek Newsletter\`
+🔖Name: ${a.name || ''}
+🧩Id: ${a.id || ''}
+👥Subscribe: ${a.subscribers || ''}
+🔗Link: ${'https://whatsapp.com/channel/' + a.invite || ''}
+⏰Date: ${new Date(a.creation_time * 1000).toLocaleString()}`
+            await ctx.sendMessage(m.chat, {
+                image: {
+                    url: "https://pps.whatsapp.net" + a.preview
+                },
+                caption
+            }, {
+                quoted: m
+            })
+        })
+    },
+};
