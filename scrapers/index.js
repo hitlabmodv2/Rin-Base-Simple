ecefbfbbf6fdf1dd743373e473af59464eefe4b7
DekuGz - Leooxzy <151708571+LeooxzyDekuu@users.noreply.github.com>
2025-05-04 16:04:24 +0700

    Update index.js

--- scrapers/index.js
+++ scrapers/index.js
@@ -1,8 +1,8 @@
 const chokidar = require("chokidar");
-const path = require('node:path');
-const fs = require('node:fs');
-const { promisify} = require('node:util');
-const chalk = require('chalk')
+const path = require("node:path");
+const fs = require("node:fs");
+const { promisify } = require("node:util");
+const chalk = require("chalk");
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 
@@ -17,63 +17,64 @@ const Scandir = async (dir) => {
   return files.reduce((a, f) => a.concat(f), []);
 };
 class Scraper {
-    #src;
-	  constructor(dir){
-		 this.dir = dir;
-		 this.#src = {}
-	  }
-	 load = async() => {
-   		let data = await Scandir('./scrapers/src')
-        for (let i of data) {
-        let name = i.split('/').pop().replace('.js', '')
-         try {
-          if (!i.endsWith(".js")) return;
-           this.#src[name] = require(i)
-          } catch(e) {
-           console.log(chalk.red.bold("- Gagal memuat Scraper :" .e));
-          delete this.#src[name]
-         }
-       }
-       return this.#src
-	 }
-	watch = async() => {
-		 const watcher = chokidar.watch(path.resolve(this.dir), {
+  #src;
+  constructor(dir) {
+    this.dir = dir;
+    this.#src = {};
+  }
+  load = async () => {
+    let data = await Scandir("./scrapers/src");
+    for (let i of data) {
+      let name = i.split("/").pop().replace(".js", "");
+      try {
+        if (!i.endsWith(".js")) return;
+        this.#src[name] = require(i);
+      } catch (e) {
+        console.log(chalk.red.bold("- Gagal memuat Scraper :" + e));
+        delete this.#src[name];
+      }
+    }
+    return this.#src;
+  };
+  watch = async () => {
+    const watcher = chokidar.watch(path.resolve(this.dir), {
       persistent: true,
       ignoreInitial: true,
     });
-       watcher.on('add', async(filename) => {
-          if (!filename.endsWith(".js")) return
-	      let name = filename.split('/').pop().replace('.js', '')
-	       if (require.cache[filename]) {
-		           delete require.cache[filename]
-		           this.#src[name] = require(filename)
-		           return this.load();
-	          }
-	         this.#src[name] = require(filename)
-           console.log(chalk.cyan.bold("- Scraper Baru telah ditambahkan : " +
-           name));
-           return this.load();
-        })
-       watcher.on('change', (filename) => {
-          if (!filename.endsWith(".js")) return
-	       let name = filename.split('/').pop().replace('.js', '')
-	          if (require.cache[filename]) {
-		           delete require.cache[filename]
-		           this.#src[name] = require(filename)
-		           return this.load();
-	          }
-	         console.log(chalk.cyan.bold('- Scraper telah diubah : ' + name))
-	           return this.load();
-	        })
-	     watcher.on("unlink", (filename) => {
-	          if (!filename.endsWith(".js")) return
-		       let name = filename.split('/').pop().replace('.js', '')
-		       delete this.#src[name]
-		       console.log(chalk.cyan.bold('- Scraper telah dihapus : ' + name))
-		       return this.load();
-	     })
-	 }
-   list = () => this.#src
+    watcher.on("add", async (filename) => {
+      if (!filename.endsWith(".js")) return;
+      let name = filename.split("/").pop().replace(".js", "");
+      if (require.cache[filename]) {
+        delete require.cache[filename];
+        this.#src[name] = require(filename);
+        return this.load();
+      }
+      this.#src[name] = require(filename);
+      console.log(
+        chalk.cyan.bold("- Scraper Baru telah ditambahkan : " + name),
+      );
+      return this.load();
+    });
+    watcher.on("change", (filename) => {
+      if (!filename.endsWith(".js")) return;
+      let name = filename.split("/").pop().replace(".js", "");
+      if (require.cache[filename]) {
+        delete require.cache[filename];
+        this.#src[name] = require(filename);
+        return this.load();
+      }
+      console.log(chalk.cyan.bold("- Scraper telah diubah : " + name));
+      return this.load();
+    });
+    watcher.on("unlink", (filename) => {
+      if (!filename.endsWith(".js")) return;
+      let name = filename.split("/").pop().replace(".js", "");
+      delete this.#src[name];
+      console.log(chalk.cyan.bold("- Scraper telah dihapus : " + name));
+      return this.load();
+    });
+  };
+  list = () => this.#src;
 }
 
-module.exports = Scraper
+module.exports = Scraper;
